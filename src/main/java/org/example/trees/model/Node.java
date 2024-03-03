6c765464e13e7b67ee9ce1da27cf7a4e5dfe69f9
jhuerta <huerta.jorge@gmail.com>
2024-03-03 11:54:56 -0600

    more algorithms

--- src/main/java/org/example/trees/model/Node.java
+++ src/main/java/org/example/trees/model/Node.java
@@ -3,10 +3,16 @@ package org.example.trees.model;
 public class Node {
 
     public int data;
+    public int depth;
     public Node left;
     public Node right;
 
     public Node(int data) {
-        this. data = data;
+        this.data = data;
+    }
+
+    public Node(int data, int depth) {
+        this.data = data;
+        this.depth = depth;
     }
 }
