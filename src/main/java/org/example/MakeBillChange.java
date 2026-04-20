cea19083f3141944c680a24ca4b2c4e42c34cbee
jhuerta <huerta.jorge@gmail.com>
2026-04-20 16:45:23 -0600

    additional method

--- src/main/java/org/example/MakeBillChange.java
+++ src/main/java/org/example/MakeBillChange.java
@@ -1,13 +1,17 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MakeBillChange {
 
-    int[] bills = { 100, 50, 20, 10, 5, 2, 1};
+    int[] bills = { 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1 };
 
     public static void main(String[] args) {
         MakeBillChange app = new MakeBillChange();
         int qty = app.makeChange(999);
         System.out.println("Smallest number of bills is: " + qty);
+        app.makeChangeWithCounter(999);
     }
 
     private int makeChange(int amount) {
@@ -22,5 +26,20 @@ public class MakeBillChange {
             }
         }
         return qty;
+    }
+
+    private void makeChangeWithCounter(int amount) {
+        if (amount < 1) {
+            return;
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int bill : bills) {
+            int qty = amount / bill;
+            if (qty > 0) {
+                map.put(bill, qty);
+                amount -= qty * bill;
+            }
+        }
+        System.out.println(map);
     }
 }
